a4c8d2438c30e2a15b86d1523369202b099282fb
Sarthik <sarthikgarg01@gmail.com>
2020-03-10 03:59:10 +0530

    added the auth and server side code for socket.io, need to write code for client side, mentioned in TODO also

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,46 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+    cookie: "socket-sid"
+});
 const path = require("path");
 const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser");
+const moragn = require("morgan");
 
 var Users = {};
-var currUser;
+var Rooms = {};
 
 const frontendFolder = path.join(__dirname, "../frontend/");
 
+/*
+    TODO: a way to store the socket of the user whenever the connection establishes
+ */
+
 app.use(cookieParser());
-app.use(express.static(frontendFolder));
+app.use(express.static(frontendFolder, { index: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(moragn("short"));
 
 app.get("/", (req, res) => {
-    // if (req.cookies.hasOwnProperty("name")) {
-    //
-    if (req.query.redirect) res.redirect("/login");
-    res.sendFile(frontendFolder + "html/index.html");
-    // } else {
-    //     console.log("here");
-    //     res.redirect("/login");
-    // }
+    console.log(req.cookies.name);
+
+    if (
+        !req.cookies.hasOwnProperty("name") &&
+        !Users.hasOwnProperty(req.cookies.name)
+    ) {
+        console.log("\n\n\nINSIDE IF\n\n\n");
+        res.redirect("/login");
+    } else {
+        console.log("\n\n\nELSE\n\n\n");
+        res.sendFile(frontendFolder + "html/index.html");
+    }
 });
 
 app.get("/login", (req, res) => {
+    console.log("\n\n\nINSIDE LOGIN\n\n\n");
     res.sendFile(frontendFolder + "html/login.html");
 });
 
@@ -44,6 +57,7 @@ app.post("/login", (req, res) => {
                     });
                 else {
                     if (result) {
+                        res.cookie("name", req.body.name);
                         res.redirect("/");
                     } else {
                         res.send({
@@ -65,22 +79,23 @@ app.post("/login", (req, res) => {
 
 app.post("/register", (req, res) => {
     if (!Users.hasOwnProperty(req.body.name)) {
-        bcrypt.hash(req.body.password, 12, (err, hash) => {
+        return bcrypt.hash(req.body.password, 12, (err, hash) => {
             if (err) {
                 return res.send({
                     msg: "Server Issues!!",
                     error: err
                 });
+            } else {
+                Users[req.body.name] = {
+                    name: req.body.name,
+                    password: hash,
+                    socket: null
+                };
+                res.cookie("name", req.body.name);
+                res.redirect("/");
             }
-
-            Users[req.body.name] = {
-                name: req.body.name,
-                password: hash
-            };
-            res.redirect("/");
         });
     } else {
-        console.log("here");
         res.send({
             msg: "User already exists",
             error:
@@ -89,8 +104,24 @@ app.post("/register", (req, res) => {
     }
 });
 
+app.post("/logout", (req, res) => {
+    console.log(req.url);
+    res.clearCookie("name");
+    res.redirect("/login");
+});
+
 io.on("connection", socket => {
-    console.log("User connected");
+    socket.on("create-room", (room, name) => {
+        console.log("User " + name + " joined room " + room);
+    });
+
+    socket.on("new-msg", (room, name, msg) => {
+        socket.to(room).broadcast.emit("new-msg", room, name, msg);
+    });
+
+    socket.on("new-user", (room, name) => {
+        socket.to(room).broadcast.emit("new-user", room, name);
+    });
 });
 
 const PORT = process.env.PORT || 4769;
