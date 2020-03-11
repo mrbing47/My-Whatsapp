9981f8569379be44c61a35894e4bda0dee1ba044
Sarthik <sarthikgarg01@gmail.com>
2020-03-11 06:02:50 +0530

    Room chat is working fine, TODOs are mentioned for the task needs to be done (like Private Messaging)

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
+/*
+    TODO Have to implement private messaging, that is what's left of the project.
+
+    ? FUTURE GOALS: Use Reactjs and MongoDB to store the chats if user wants and also deletes them
+
+*/
+
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
-    cookie: "socket-sid"
+	cookie: "socket-sid"
 });
 const path = require("path");
 const bcrypt = require("bcryptjs");
@@ -10,13 +17,9 @@ const cookieParser = require("cookie-parser");
 const moragn = require("morgan");
 
 var Users = {};
-var Rooms = {};
+var Rooms = new Set();
 
 const frontendFolder = path.join(__dirname, "../frontend/");
-
-/*
-    TODO: a way to store the socket of the user whenever the connection establishes
- */
 
 app.use(cookieParser());
 app.use(express.static(frontendFolder, { index: false }));
@@ -25,106 +28,128 @@ app.use(express.urlencoded({ extended: true }));
 app.use(moragn("short"));
 
 app.get("/", (req, res) => {
-    console.log(req.cookies.name);
-
-    if (
-        !req.cookies.hasOwnProperty("name") &&
-        !Users.hasOwnProperty(req.cookies.name)
-    ) {
-        console.log("\n\n\nINSIDE IF\n\n\n");
-        res.redirect("/login");
-    } else {
-        console.log("\n\n\nELSE\n\n\n");
-        res.sendFile(frontendFolder + "html/index.html");
-    }
+	if (req.cookies.name && Users[req.cookies.name]) {
+		res.sendFile(frontendFolder + "html/index.html");
+	} else {
+		res.redirect("/login");
+	}
 });
 
 app.get("/login", (req, res) => {
-    console.log("\n\n\nINSIDE LOGIN\n\n\n");
-    res.sendFile(frontendFolder + "html/login.html");
+	res.sendFile(frontendFolder + "html/login.html");
 });
 
 app.post("/login", (req, res) => {
-    if (Users.hasOwnProperty(req.body.name)) {
-        bcrypt.compare(
-            req.body.password,
-            Users[req.body.name].password,
-            (err, result) => {
-                if (err)
-                    res.send({
-                        msg: "Server Error",
-                        error: err
-                    });
-                else {
-                    if (result) {
-                        res.cookie("name", req.body.name);
-                        res.redirect("/");
-                    } else {
-                        res.send({
-                            msg: "Wrong Credentials",
-                            error:
-                                "wrong input password, check you password carefully"
-                        });
-                    }
-                }
-            }
-        );
-    } else {
-        res.send({
-            msg: "Wrong Credentials",
-            error: "No user was found in the database, try registering yourself"
-        });
-    }
+	if (Users[req.body.name]) {
+		bcrypt.compare(
+			req.body.password,
+			Users[req.body.name].password,
+			(err, result) => {
+				if (err)
+					res.send({
+						msg: "Server Error",
+						error: err
+					});
+				else {
+					if (result) {
+						res.cookie("name", req.body.name);
+						res.redirect("/");
+					} else {
+						res.send({
+							msg: "Wrong Credentials",
+							error:
+								"wrong input password, check you password carefully"
+						});
+					}
+				}
+			}
+		);
+	} else {
+		res.send({
+			msg: "Wrong Credentials",
+			error: "No user was found in the database, try registering yourself"
+		});
+	}
 });
 
 app.post("/register", (req, res) => {
-    if (!Users.hasOwnProperty(req.body.name)) {
-        return bcrypt.hash(req.body.password, 12, (err, hash) => {
-            if (err) {
-                return res.send({
-                    msg: "Server Issues!!",
-                    error: err
-                });
-            } else {
-                Users[req.body.name] = {
-                    name: req.body.name,
-                    password: hash,
-                    socket: null
-                };
-                res.cookie("name", req.body.name);
-                res.redirect("/");
-            }
-        });
-    } else {
-        res.send({
-            msg: "User already exists",
-            error:
-                "provided username already exists in the database, try to login"
-        });
-    }
+	if (!Users[req.body.name]) {
+		return bcrypt.hash(req.body.password, 12, (err, hash) => {
+			if (err) {
+				return res.send({
+					msg: "Server Issues!!",
+					error: err
+				});
+			} else {
+				Users[req.body.name] = {
+					name: req.body.name,
+					password: hash,
+					socket: undefined
+				};
+				res.cookie("name", req.body.name);
+				res.redirect("/");
+			}
+		});
+	} else {
+		res.send({
+			msg: "User already exists",
+			error:
+				"provided username already exists in the database, try to login"
+		});
+	}
 });
 
 app.post("/logout", (req, res) => {
-    console.log(req.url);
-    res.clearCookie("name");
-    res.redirect("/login");
+	console.log(req.url);
+	res.clearCookie("name");
+	res.redirect("/login");
 });
 
 io.on("connection", socket => {
-    socket.on("create-room", (room, name) => {
-        console.log("User " + name + " joined room " + room);
-    });
+	console.log("User connected " + socket.id);
 
-    socket.on("new-msg", (room, name, msg) => {
-        socket.to(room).broadcast.emit("new-msg", room, name, msg);
-    });
+	socket.on("user-login", name => {
+		Users[name]["socket"] = socket;
+	});
 
-    socket.on("new-user", (room, name) => {
-        socket.to(room).broadcast.emit("new-user", room, name);
-    });
+	socket.on("create-room", (prevRoom, currRoom, name, ack) => {
+		console.log("here");
+
+		if (!Rooms.has(currRoom)) {
+			if (prevRoom !== "") socket.leave(prevRoom);
+			socket.join(currRoom);
+
+			Rooms.add(currRoom);
+
+			console.log("User " + name + " joined room " + currRoom);
+
+			ack("OK");
+		} else {
+			ack("Room already exists!!!");
+		}
+	});
+
+	socket.on("new-msg", (room, name, msg) => {
+		socket.to(room).broadcast.emit("new-msg", name, msg);
+	});
+
+	socket.on("user-join", (prevRoom, currRoom, name, ack) => {
+		if (Rooms.has(currRoom)) {
+			if (prevRoom !== "") socket.leave(prevRoom);
+			socket.join(currRoom);
+
+			socket.to(currRoom).broadcast.emit("user-join", name);
+
+			ack("OK");
+		} else {
+			ack("No Room was found!!!");
+		}
+	});
+
+	socket.on("user-leave", (currRoom, name) => {});
 });
 
 const PORT = process.env.PORT || 4769;
 http.listen(PORT, () => {
-    console.log("Listening to PORT " + PORT);
+	console.log("Listening to PORT " + PORT);
 });
